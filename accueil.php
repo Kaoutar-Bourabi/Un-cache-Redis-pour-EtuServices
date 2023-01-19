fe5a739a255c77d232552cfebe68c2997f1f685d
Kaoutar-Bourabi <72504063+Kaoutar-Bourabi@users.noreply.github.com>
2023-01-19 23:04:37 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>
+            Accueil EtuServices
+        </title>
+
+    </head>
+
+    <body>
+        <center>
+        <br><br>Bienvenue sur <i>EtuServices</i><br><br>
+
+        Vous pouvez vous connecter en cliquant <a href="login.php">ici</a>
+
+        </center>
+
+
+    </body>
+
+</html>
